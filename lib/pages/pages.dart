79dfaa3ff96b484bce8b3007fca10be47977547b
anhquantr123 <anhquan.lien123@gmail.com>
2024-09-25 07:22:46 +0700

    init app

---
+++ lib/pages/pages.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+export 'intro_page.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
